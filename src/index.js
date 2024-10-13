50a0066afc79104592d10039b350848204e48e62
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-13 10:29:43 +0300

    Config express and handlebars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
 
 const app = express();
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs',
+}));
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
 
 app.get('/', (req, res) => {
   res.send('<h1>Welcome my friend</h1>')
