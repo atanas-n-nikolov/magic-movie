0ae68c41b86bf006ef6392a4b361be66f58870cd
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-13 10:49:20 +0300

    Render first view

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import handlebars from 'express-handlebars';
+import { Router } from 'express';
 
 const app = express();
+const router = Router();
+
 app.engine('hbs', handlebars.engine({
   extname: 'hbs',
 }));
 app.set('view engine', 'hbs');
 app.set('views', './src/views');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('<h1>Welcome my friend</h1>')
+  res.render('home');
 });
 
 app.listen(5000, () => console.log('Server is listening on port: http://localhost:5000 ...'));
