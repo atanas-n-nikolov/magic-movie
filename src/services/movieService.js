f2ddc2ad0f20afead2c52ddf53bb2023effca7e5
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-14 18:13:37 +0300

    Show cast in details page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -18,12 +18,17 @@ const getAll = async (filter = {}) => {
   return movies;
 };
 
-const getOne = (movieId) => Movie.findById(movieId);
+const getOne = (movieId) => Movie.findById(movieId).populate('casts');
 
 const create = (movie) => Movie.create(movie);
+
+const attach = (movieId, castId) => {
+  return Movie.findByIdAndUpdate(movieId, { $push: { casts: castId } });
+};
 
 export default {
   getAll,
   create,
-  getOne
+  getOne,
+  attach
 };
