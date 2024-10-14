c8e94b9d21b4bdc8839f38e0045af493ff528c9e
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-14 15:58:16 +0300

    Add cast model and render page

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import homeController from './controllers/homeController.js';
 import movieController from './controllers/movieController.js';
+import castCotroller from './controllers/castController.js';
 
 const router = Router();
 
 router.use(homeController);
 router.use('/movies', movieController);
+router.use('/casts', castCotroller);
 router.all('*', (req, res) => {
   res.render('404');
 });
