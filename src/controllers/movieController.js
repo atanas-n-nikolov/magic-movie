e6d00dfb4173d8828325dc2f8148452fde093d96
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-14 08:13:56 +0300

    Fix details page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -16,7 +16,7 @@ router.post('/create', async (req, res) => {
 
 router.get('/:movieId/details', async (req, res) => {
   const movieId = req.params.movieId;
-  const movie = await movieService.getOne(movieId);
+  const movie = await movieService.getOne(movieId).lean();
   movie.ratingView = getRatingViewData(movie.rating);
 
   res.render('movies/details', { movie });
