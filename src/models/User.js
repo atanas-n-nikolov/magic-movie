b32a33b54ca19b3b334316a2e09f697bfca713c6
atanas-n-nikolov <156112757+atanas-n-nikolov@users.noreply.github.com>
2024-10-15 09:32:31 +0300

    Hash password before save

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,17 @@
 import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt';
+
+const SALT_ROUND = 10;
 
 const userSchema = new Schema({
   user: String,
   password: String,
+});
+
+userSchema.pre('save', async function() {
+  const hash = await bcrypt.hash(this.password, SALT_ROUND);
+
+  this.password = hash;
 });
 
 const User = model('User', userSchema);
